b7031757930c0af8bf96667960f267f9ef537908
Felipe Moraes <felipe3902@gmail.com>
2025-08-15 00:24:37 -0300

    fix: ajustes no balenceamento das missoes

--- server/gameConfig.js
+++ server/gameConfig.js
@@ -19,20 +19,26 @@ const DEFAULT_MAX_ROUNDS = 10; // Rodadas máximas (para modo fixedRounds)
 // Possibilidades de missões individuais e coletivas com suas recompensas, prejuízos e dificuldade
 const missionsData = {
   individual: [
-    { name: "Contrabando de Especiarias", reward: 70, failureCost: 30, difficulty: 7 },
-    { name: "Transporte de Armas", reward: 120, failureCost: 50, difficulty: 9 },
-    { name: "Roubo de Dados", reward: 100, failureCost: 40, difficulty: 8 },
-    { name: "Contrabando de Especiarias", reward: 80, failureCost: 30, difficulty: 7 },
-    { name: "Transporte de Armas", reward: 110, failureCost: 50, difficulty: 9 },
-    { name: "Roubo de Dados", reward: 110, failureCost: 40, difficulty: 8 }
+    { name: "Contrabando de Especiarias", reward: 100, failureCost: 30, difficulty: 5 },
+    { name: "Transporte de Armas", reward: 150, failureCost: 50, difficulty: 7 },
+    { name: "Roubo de Dados", reward: 120, failureCost: 40, difficulty: 6 },
+    { name: "Tráfico de Relíquias", reward: 70, failureCost: 30, difficulty: 7 },
+    { name: "Rota de Armas", reward: 120, failureCost: 50, difficulty: 9 },
+    { name: "Invasão de Sistema", reward: 100, failureCost: 40, difficulty: 8 },
+    { name: "Esquema de Roubos", reward: 80, failureCost: 30, difficulty: 7 },
+    { name: "Entrega Secreta", reward: 110, failureCost: 50, difficulty: 9 },
+    { name: "Hackeamento Global", reward: 110, failureCost: 40, difficulty: 8 }
   ],
   collective: [
-    { name: "Resgate de Reféns", reward: 400, failureCost: 60, difficulty: 11 },
-    { name: "Sabotagem Corporativa", reward: 350, failureCost: 55, difficulty: 10 },
-    { name: "Destruição de Base Militar", reward: 370, failureCost: 70, difficulty: 13 },
-    { name: "Resgate de Reféns", reward: 450, failureCost: 60, difficulty: 15 },
-    { name: "Sabotagem Corporativa", reward: 310, failureCost: 55, difficulty: 10 },
-    { name: "Destruição de Base Militar", reward: 420, failureCost: 70, difficulty: 13 }
+    { name: "Resgate de Reféns", reward: 200, failureCost: 60, difficulty: 8 },
+    { name: "Sabotagem Corporativa", reward: 180, failureCost: 55, difficulty: 7 },
+    { name: "Destruição de Base Militar", reward: 220, failureCost: 70, difficulty: 9 },
+    { name: "Operação Libertação", reward: 400, failureCost: 60, difficulty: 11 },
+    { name: "Ataque Interno", reward: 350, failureCost: 55, difficulty: 10 },
+    { name: "Impacto Tático", reward: 370, failureCost: 70, difficulty: 13 },
+    { name: "Ataque Cibernético", reward: 450, failureCost: 60, difficulty: 15 },
+    { name: "Vingança Corporativa", reward: 310, failureCost: 55, difficulty: 10 },
+    { name: "Desmantelamento Estratégico", reward: 420, failureCost: 70, difficulty: 13 }
   ]
 };
 
